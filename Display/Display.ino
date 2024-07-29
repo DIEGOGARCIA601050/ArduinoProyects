f9a63eac11a73a08c0f3fc9a814bda909024ff67
DiegoAdmin <diegoadmin@Lenovo-Ideapad.ipv6.infinitum.net.mx>
2024-07-29 10:17:42 -0600

    UpdateDisplay

--- Display/Display.ino
+++ Display/Display.ino
@@ -15,10 +15,74 @@ void setup() {
   pinMode(F,OUTPUT);
   pinMode(G,OUTPUT);
   pinMode(9,OUTPUT);
-
+  
 }
 
+void LetterA() {
+  for(int i=2;i<9;i++){
+    if(i!=5) {
+      digitalWrite(i,HIGH);
+      delay(500);
+    }
+  }
+  for(int i=2;i<9;i++){
+    if(i!=5) {
+      digitalWrite(i,LOW);
+      delay(500);
+    }
+  }
+}
+void Letterb() {
+  for(int i=2;i<=8;i++){
+    if(i!=5) {
+      if(i!=6){
+        
+        digitalWrite(i,HIGH);
+        delay(500);
+      }
+    }
+  }
+  for(int i=2;i<=8;i++){
+    if(i!=5) {
+      
+      digitalWrite(i,LOW);
+      delay(500);
+    }
+  }
+}
+void LetterE(){
+    for(int i=2;i<9;i++){
+      if(i!=6) {
+        if(i!=7) {
+          digitalWrite(i,HIGH);
+          delay(500);
+        }
+      }
+  }
+  for(int i=2;i<9;i++){
+    if(i!=6) {
+      if(i!=7) {
+        digitalWrite(i,LOW);
+      delay(500);
+      }
+    }
+  }
+}
+void LetterC(){
+    for(int i=2;i<6;i++){
+      digitalWrite(i,HIGH);
+      delay(500);
+    }
+  for(int i=2;i<6;i++){
+    digitalWrite(i,LOW);
+    delay(500);
+  }
+}
 void loop() {
+  LetterA();
+  Letterb();
+  LetterE();
+  LetterC();
   for(int i=2;i<=9;i++){
     digitalWrite(i,HIGH);
     delay(1000);
---
+++ git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git add .
+read -p "Mensaje: " msg
+git commit -m $msg
+
